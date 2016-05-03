ba97a3c868f6a84819ccee5ce7733bfab68b1bf7
z3635363 <245767493@qq.com>
2016-05-04 00:17:53 +0800

    alloc

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -40,6 +40,7 @@ team_t team = {
 
 #define WSIZE 4
 #define DSIZE 8
+
 #define CHUNKSIZE (1 << 12)
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
@@ -77,23 +78,105 @@ int mm_init(void)
 		return -1;
 	PUT(heap_listtp, 0);
 	PUT(heap_listtp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listtp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listtp + (3 * WSIZE), PACK(0 , 1));
+    heap_listtp += (2 * WSIZE);
     return 0;
+}
+
+static void *coalesce(void *bp) {
+    size_t pre_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+    if(pre_alloc && next_alloc) {
+        return bp;
+    } else if(pre_alloc && !next_alloc) {
+        // 要释放的块在中间
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    } else if(!pre_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    } else {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+                GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
+static void *extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+    size = (words % 2)? (words + 1) * WSIZE : words * WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1) {
+        return NULL;
+    }
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));// 尾节点
+    return coalesce(bp);
 }
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
+
+void *find_fit(size_t size) {
+    void *head = heap_listtp;
+    while(GET_SIZE(NEXT_BLKP(head)) != 0) {
+        if(!GET_ALLOC(head) && GET_SIZE(head) > size) {
+            return head;
+        } else
+            head = NEXT_BLKP(head);
+    }
+    return NULL;
+}
+
+void place(void *head, size_t size) {
+    size_t origin_size = GET_SIZE(head);
+    PUT(HDRP(head), PACK(ALIGN(size), 1));
+    PUT(FTRP(head), PACK(ALIGN(size), 1));
+    size_t left = origin_size - ALIGN(size);
+    PUT(HDRP(NEXT_BLKP(head)), PACK(left, 0));
+    PUT(FTRP(NEXT_BLKP(head)), PACK(left, 0));
+}
+
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+//    int newsize = ALIGN(size + SIZE_T_SIZE);
+//    void *p = mem_sbrk(newsize);
+//    if (p == (void *)-1)
+//	return NULL;
+//    else {
+//        *(size_t *)p = size;
+//        return (void *)((char *)p + SIZE_T_SIZE);
+//    }
+    size_t asize;
+    size_t extendsize;
+    char *bp;
+
+    if(size == 0)
+        return NULL;
+    if(size < DSIZE)
+        asize = 2 * DSIZE;
+    else
+        asize = DSIZE * ( ( size + (DSIZE) + (DSIZE - 1) ) / DSIZE );
+    if((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
     }
+    extendsize = MAX(asize, CHUNKSIZE);
+    if((bp = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
 }
 
 /*
@@ -101,6 +184,10 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(ptr));
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
 }
 
 /*
